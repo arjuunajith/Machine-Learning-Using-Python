41fd5e486353721efa66f64d1609b667b9f10bc2
Arjun Ajith <arjuunajith@gmail.com>
2020-06-15 20:31:56 +0530

    backend tensorflow

--- Neural Network with Keras.py
+++ Neural Network with Keras.py
@@ -5,6 +5,22 @@ from keras.layers import Dense
 # Loading the dataset
 dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
 
-# Splitting into input (X) and output (y) variables
+# Splitting into input and output variables
 X = dataset[:, 0:8]
 y = dataset[:, 8]
+
+# Defining the keras model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Fitting the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+
+# Evaluating the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
